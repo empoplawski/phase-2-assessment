d93b9f6c9d4dd1baac795c9160c232cefbc2c190
Elise Poplawski <elise.poplawski@gmail.com>
2017-11-15 10:07:29 -0600

    finsished with all passing tests

--- part-1/classroom.js
+++ part-1/classroom.js
@@ -2,3 +2,27 @@
 /*   It is required to use Array.prototype methods          */
 /*   when iterating over a classroom's array of students.   */
 /************************************************************/
+
+var Classroom = function(students){
+  this.students = students
+}
+
+Classroom.prototype.find = function(name){
+
+  function findStudentName(student){
+    console.log("im here")
+    return student.firstName === name;
+  }
+  return this.students.find(findStudentName)
+}
+
+
+Classroom.prototype.honorRollStudents = function(){
+  var honorStudents = this.students.filter(function(student){
+      console.log("in function")
+      return student.averageScore() >= 95
+  })
+  return honorStudents
+}
+
+
